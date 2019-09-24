6e2ff6bc6cac611ecda74a7814d97edc5dd93703
Greg Kempe <greg@kempe.net>
2019-09-24 09:48:04 +0200

    Mirror targets, cross-account mirror

--- service.py
+++ service.py
@@ -7,6 +7,7 @@ import requests
 API_AUTH_TOKEN = os.environ['API_AUTH_TOKEN']
 API_URL = os.environ.get('API_URL', 'https://api.gazettes.laws.africa/v1')
 TIMEOUT = 30
+MIRROR_TARGETS = os.environ.get('MIRROR_TARGETS', '').split(' ')
 
 session = requests.Session()
 session.headers.update({'Authorization': 'Token %s' % API_AUTH_TOKEN})
@@ -75,31 +76,47 @@ def csv_from_s3(bucket, key):
 def archived_gazette_changed(event, context):
     """ S3 event. Archived gazette has been created or deleted.
     """
-    # TODO: appropriate creds and permissions
-    s3 = boto3.client('s3')
-    tgt_bucket = "lawsafrica-gazettes-archive"
 
     for record in event['Records']:
         print("Got S3 event record: {}".format(record))
 
         bucket = record['s3']['bucket']['name']
         key = record['s3']['object']['key'].replace('+', ' ')
-        if not key.startswith('archive/'):
+        if not key.startswith('test/'):
             continue
-        tgt_key = "test-output/" + key[8:]
 
-        print("Bucket: {}, Key: {}".format(bucket, key))
+        for access_key, secret_key, tgt_bucket, tgt_prefix in get_mirror_targets():
+            s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+            tgt_key = tgt_prefix + key[8:]
 
-        if record['eventName'].startswith('ObjectRemoved'):
-            print("Deleting bucket: {}, key: {}".format(tgt_bucket, tgt_key))
-            s3.delete_object(Bucket=bucket, Key=key)
+            print("Mirror from bucket: {}, key: {} to bucket: {}, key: {}".format(bucket, key, tgt_bucket, tgt_key))
 
-        if record['eventName'].startswith('ObjectCreated'):
-            print("Copying to bucket: {}, key: {}".format(tgt_bucket, tgt_key))
-            s3.copy_object(
-                Bucket=tgt_bucket,
-                Key=tgt_key,
-                CopySource={
-                    'Bucket': bucket,
-                    'Key': key,
-                })
+            if record['eventName'].startswith('ObjectRemoved'):
+                print("Deleting bucket: {}, key: {}".format(tgt_bucket, tgt_key))
+                s3.delete_object(Bucket=bucket, Key=key)
+
+            if record['eventName'].startswith('ObjectCreated'):
+                print("Copying to bucket: {}, key: {}".format(tgt_bucket, tgt_key))
+                with tempfile.TemporaryFile() as f:
+                    s3.download_fileobj(bucket, key, f)
+                    f.seek(0)
+                    s3.upload_fileobj(f, tgt_bucket, tgt_key)
+
+
+def get_mirror_targets():
+    targets = []
+
+    for target in MIRROR_TARGETS:
+        # access-key:secret-key@bucket/prefix
+        creds, loc = target.split('@')
+        creds = creds.split(':')
+
+        if '/' in loc:
+            bucket, prefix = loc.split('/', 1)
+        else:
+            bucket = loc
+            prefix = ''
+
+        targets.append([creds[0], creds[1], bucket, prefix])
+
+    return targets
