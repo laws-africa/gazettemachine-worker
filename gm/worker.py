859f068905fd39c8269c331a5a3a5d85bcd2d829
Greg Kempe <greg@kempe.net>
2023-03-11 11:22:31 +0200

    update worker

--- gm/worker.py
+++ gm/worker.py
@@ -30,78 +30,71 @@ class Worker:
         url = GM_API_URL + path
 
         info = self.info_from_gm(url)
-        self.fetch(info)
-        self.ocr_to_s3(info)
+        self.fetch(info["s3_location"])
+        info["s3_location"] = self.ocr_to_s3(info["s3_location"])
         self.update_gm(info, url)
 
     def info_from_gm(self, url):
-        log.info("Fetching info from {}".format(url))
+        log.info(f"Fetching info from {url}")
         resp = requests.get(url, timeout=TIMEOUT, headers=self.headers)
         resp.raise_for_status()
 
         info = resp.json()
-        log.info("Received: {}".format(info))
+        log.info(f"Received: {info}")
         return info
 
-    def fetch(self, info):
-        log.info("Dowloading {} from S3".format(info['s3_location']))
+    def fetch(self, s3_location):
+        log.info(f"Dowloading {s3_location} from S3")
 
         tmp = tempfile.NamedTemporaryFile()
-        bucket, key = info['s3_location'].split('/', 1)
+        bucket, key = s3_location.split('/', 1)
         self.s3.download_fileobj(bucket, key, tmp)
         tmp.flush()
         tmp.seek(0)
 
         self.tmpfile = tmp
 
-    def ocr_to_s3(self, info):
-        """ OCR the file in f, write it back into f AND to S3, and update
-        info to reflect the new file, moving the original into the sources list.
-        """
+    def ocr_to_s3(self, s3_location):
+        """ OCR the file in f, write it back into f AND to S3, returning the new s3 location. """
         # OCR the file in place
         self.ocr_file(self.tmpfile.name)
         self.tmpfile.flush()
 
         self.tmpfile.seek(0, 2)
-        info['size'] = self.tmpfile.tell()
         self.tmpfile.seek(0)
 
-        key = info['s3_location'].split('/', 1)[1]
-        ocr_key = '{}{}-ocr.pdf'.format(self.TEMP_PATH, key)
-        ocr_location = '{}/{}'.format(self.INCOMING_BUCKET, ocr_key)
+        key = s3_location.split('/', 1)[1]
+        ocr_key = f'{self.TEMP_PATH}{key}-ocr.pdf'
+        ocr_location = f'{self.INCOMING_BUCKET}/{ocr_key}'
 
-        log.info("Uploading OCRd file to {}".format(ocr_location))
+        log.info(f"Uploading OCRd file to {ocr_location}")
         self.s3.put_object(Bucket=self.INCOMING_BUCKET, Key=ocr_key, Body=self.tmpfile)
-
-        if not info.get('sources'):
-            info['sources'] = []
-        info['sources'].append(info['s3_location'])
-        info['s3_location'] = ocr_location
-        info['ocrd'] = True
-        info['coverpage_text'] = None
+        return ocr_location
 
     def ocr_file(self, target):
         with tempfile.TemporaryDirectory() as tmpdir:
             # make a multipage tiff of the original PDF
-            tiffs = "{}/images.tiff".format(tmpdir)
+            tiffs = f"{tmpdir}/images.tiff"
             result = subprocess.run(["gs", "-o", tiffs, "-sDEVICE=tiff32nc", "-dUseBigTIFF=true", "-r300", self.tmpfile.name])
             result.check_returncode()
 
             # OCR using tesseract to produce a pdf
-            pdf = "{}/ocr-output".format(tmpdir)
+            pdf = f"{tmpdir}/ocr-output"
             result = subprocess.run(["tesseract", tiffs, pdf, "pdf"])
             result.check_returncode()
 
             # convert images in resulting PDF to reduce size
             result = subprocess.run([
                 "gs", "-dNOPAUSE", "-dBATCH", "-sDEVICE=pdfwrite",
-                "-dCompatibilityLevel=1.4", "-dPDFSETTINGS=/ebook", "-sOutputFile={}".format(target), "{}.pdf".format(pdf)])
+                "-dCompatibilityLevel=1.4", "-dPDFSETTINGS=/ebook",
+                f"-sOutputFile={target}", f"{pdf}.pdf"])
+            result.check_returncode()
 
     def update_gm(self, info, url):
-        log.info("Updating GM {}: {}".format(url, info))
+        log.info(f"Updating GM {url}: {info}")
         resp = requests.put(url, timeout=TIMEOUT, json=info, headers=self.headers)
-        log.info("Response: %s" % resp.text)
+        log.info(f"Response: {resp.text}")
         resp.raise_for_status()
 
         info = resp.json()
-        log.info("Received: {}".format(info))
+        log.info(f"Received: {info}")
